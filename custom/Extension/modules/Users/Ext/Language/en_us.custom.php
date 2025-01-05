9f8dc0826be27cfe545f8c58dab274157a21f50d
junaidgemini <junaid.gemini24@gmail.com>
2025-01-05 23:44:43 +0500

    replacemnt _C in users Self-Referential Relate Field

--- custom/Extension/modules/Users/Ext/Language/en_us.custom.php
+++ custom/Extension/modules/Users/Ext/Language/en_us.custom.php
@@ -4,6 +4,10 @@ $mod_strings['LBL_JD_BRANCH_STATE'] = 'Branch State';
 $mod_strings['LBL_JD_BRANCH'] = 'Branch';
 $mod_strings['LBL_JD_ROLES_C'] = 'Role';
 $mod_strings['LBL_JD_REPORT_GROUP'] = 'Report Group';
+$mod_strings['LBL_JD_REPLACEMENT_C'] = 'Replacement';
+$mod_strings['LBL_JD_REPLACEMENT_C_NAME'] = 'Replacement';
+$mod_strings['LBL_JD_USERS_JD_REPLACEMENT_C_LINK'] = 'Replacement';
+
 // $mod_strings['LBL_JD_ROLE_ID'] = 'Role ID';
 // $mod_strings['LBL_JD_ROLE_NAME'] = 'Role Name';
 // $mod_strings['LBL_JD_USERS_ROLES_LINK'] = 'Role Link';
---
+++ custom/Extension/modules/Users/Ext/Vardefs/sugarfield_jd_replacement.php
@@ -0,0 +1,57 @@
+<?php
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+// Create a Self-Referential Relate Field:
+//ID
+$dictionary['User']['fields']['jd_replacement_c'] = array(
+    'name' => 'jd_replacement_c',
+    'rname' => 'id',
+    'vname' => 'LBL_JD_REPLACEMENT_C',
+    'type' => 'id',
+    'table' => 'users',
+    'isnull' => 'true',
+    'module' => 'Users',
+    'dbType' => 'id',
+    'reportable' => false,
+    'audited' => true,
+    'duplicate_merge' => 'disabled',
+    'required' => false
+);
+//Name
+$dictionary['User']['fields']['jd_replacement_c_name'] = array(
+    'name' => 'jd_replacement_c_name',
+    'rname' => 'name',
+    'id_name' => 'jd_replacement_c',
+    'vname' => 'LBL_JD_REPLACEMENT_C_NAME',
+    'type' => 'relate',
+    'link' => 'jd_users_jd_replacement_c_link',
+    'table' => 'users',
+    'isnull' => 'true',
+    'module' => 'Users',
+    'source' => 'non-db',
+    'required' => false,
+);
+
+//Link
+$dictionary['User']['fields']['jd_users_jd_replacement_c_link'] = array(
+    'name'  =>  'jd_users_jd_replacement_c_link',
+    'type'  =>  'link',
+    'relationship'  =>  'jd_users_jd_replacement_c_rel',
+    'module' => 'Users',
+    'bean_name' => 'User',
+    'source' => 'non-db',
+    'vname' => 'LBL_JD_USERS_JD_REPLACEMENT_C_LINK',
+);
+
+
+//Relationship
+$dictionary["User"]["relationships"]["jd_users_jd_replacement_c_rel"] = array(
+    'lhs_module' => 'Users',
+    'lhs_table' =>  'users',
+    'lhs_key' => 'id',
+    'rhs_module' => 'Users',
+    'rhs_table' =>  'users',
+    'rhs_key' =>  'jd_replacement_c',
+    'relationship_type' =>  'one-to-many'
+);
