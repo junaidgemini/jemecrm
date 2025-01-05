1c6938c82a80f4b334f8ae9b74141cbbf7749cde
junaidgemini <junaid.gemini24@gmail.com>
2025-01-05 21:10:06 +0500

    deposit valid %

--- custom/Extension/modules/jd_cso_monthly_preformance/Ext/Language/en_us.lang.php
+++ custom/Extension/modules/jd_cso_monthly_preformance/Ext/Language/en_us.lang.php
@@ -41,3 +41,5 @@ $mod_strings['LBL_JD_DISBURSEMENT_VALID_PERCENT'] = 'Disbursement Valid (%)';
 $mod_strings['LBL_JD_LOAN_PORTFOLIO_VALID_PERCENT'] = 'Loan Portfolio Valid (%)';
 $mod_strings['LBL_JD_NEW_CLIENT_VALID_PERCENT'] = 'New Client Valid (%)';
 $mod_strings['LBL_JD_DEPOSIT_MOBILISE_VALID_PERCENT'] = 'Deposit Mobilised Valid (%)';
+
+$mod_strings['LBL_JD_DEPOSIT_MOBILISE_VALID_PERCENT'] = 'Deposit Balance Valid(%)';
---
+++ custom/Extension/modules/jd_cso_monthly_preformance/Ext/Vardefs/sugarfield_jd_deposit_balance_valid_percent.php
@@ -0,0 +1,15 @@
+<?php
+// jd_deposit_balance_valid_percent
+$dictionary['jd_cso_monthly_preformance']['fields']['jd_deposit_balance_valid_percent'] = array(
+	'name' => 'jd_deposit_balance_valid_percent',
+	'vname' => 'LBL_JD_DEPOSIT_BALANCE_VALID_PERCENT',
+    'type' => 'decimal', // Specifies the field type as decimal
+    'len' => '18',       // Total length of the number
+    'precision' => '2',  // Number of decimal places
+	'required' => true,
+    'comment' => '',
+    'importable' => 'true',
+    'audited' => true,
+    'reportable' => true,
+);
+?>
