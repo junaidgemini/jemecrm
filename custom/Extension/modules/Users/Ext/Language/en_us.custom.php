cb179073b6acab7ada14042de6b27571cbf1793b
junaidgemini <junaid.gemini24@gmail.com>
2025-01-05 13:38:30 +0500

    role group

--- custom/Extension/modules/Users/Ext/Language/en_us.custom.php
+++ custom/Extension/modules/Users/Ext/Language/en_us.custom.php
@@ -3,6 +3,7 @@ $mod_strings['LBL_JD_ZONE'] = 'Zone';
 $mod_strings['LBL_JD_BRANCH_STATE'] = 'Branch State';
 $mod_strings['LBL_JD_BRANCH'] = 'Branch';
 $mod_strings['LBL_JD_ROLES_C'] = 'Role';
+$mod_strings['LBL_JD_REPORT_GROUP'] = 'Report Group';
 // $mod_strings['LBL_JD_ROLE_ID'] = 'Role ID';
 // $mod_strings['LBL_JD_ROLE_NAME'] = 'Role Name';
 // $mod_strings['LBL_JD_USERS_ROLES_LINK'] = 'Role Link';
---
+++ custom/Extension/modules/Users/Ext/Vardefs/sugarfield_jd_report_group.php
@@ -0,0 +1,12 @@
+<?php
+$dictionary['User']['fields']['jd_report_group'] = array(
+	'name' => 'jd_report_group',
+	'vname' => 'LBL_JD_REPORT_GROUP',
+	'type' => 'varchar',
+	'importable' => 'true',
+	'duplicate_merge' => 'disabled',
+	'duplicate_merge_dom_value' => '0',
+	'audited' => true,
+	'reportable' => true,
+);
+?>
