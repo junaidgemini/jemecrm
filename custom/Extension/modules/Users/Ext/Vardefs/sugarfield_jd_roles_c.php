c9c4aa8db3478c79784789ef8c1edbdccaa675a8
junaidgemini <junaid.gemini24@gmail.com>
2025-01-05 13:18:40 +0500

    roles dom in users

---
+++ custom/Extension/modules/Users/Ext/Vardefs/sugarfield_jd_roles_c.php
@@ -0,0 +1,16 @@
+<?php
+$dictionary['User']['fields']['jd_roles_c'] = array(
+	'name' => 'jd_roles_c',
+	'vname' => 'LBL_JD_ROLES_C',
+	'type' => 'enum',
+	'options' => 'jd_roles_c_dom',
+	'massupdate' => 0,
+	'comments' => '',
+	'help' => '',
+	'importable' => 'true',
+	'duplicate_merge' => 'disabled',
+	'duplicate_merge_dom_value' => '0',
+	'audited' => true,
+	'reportable' => true,
+);
+?>
