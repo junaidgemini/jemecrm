7811c2cb30e77fa77f5715dd211be9a455441f11
junaidgemini <junaid.gemini24@gmail.com>
2025-01-05 19:20:15 +0500

    Disbursement(%)

---
+++ custom/Extension/modules/jd_cso_monthly_preformance/Ext/Vardefs/sugarfield_jd_disbursement_valid_percent.php
@@ -0,0 +1,14 @@
+<?php
+$dictionary['jd_cso_monthly_preformance']['fields']['jd_disbursement_valid_percent'] = array(
+	'name' => 'jd_disbursement_valid_percent',
+	'vname' => 'LBL_JD_DISBURSEMENT_VALID_PERCENT',
+    'type' => 'decimal', // Specifies the field type as decimal
+    'len' => '18',       // Total length of the number
+    'precision' => '2',  // Number of decimal places
+	'required' => true,
+    'comment' => '',
+    'importable' => 'true',
+    'audited' => true,
+    'reportable' => true,
+);
+?>
