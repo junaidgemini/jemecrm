05b155894a34f2a6ad2796759ea726dcc72fd5a0
junaidgemini <junaid.gemini24@gmail.com>
2025-01-23 21:32:50 +0500

    users branch email

---
+++ custom/Extension/modules/Users/Ext/Vardefs/sugarfield_branch_emails.php
@@ -0,0 +1,19 @@
+<?php
+$dictionary['User']['fields']['branch_emails'] = array(
+	'name' => 'branch_emails',
+	'vname' => 'LBL_BRANCH_EMAILS',
+	'type' => 'enum',
+	'options' => 'branch_emails_dom',
+	'massupdate' => 0,
+	'comments' => '',
+	'help' => '',
+	'importable' => 'true',
+	'duplicate_merge' => 'disabled',
+	'duplicate_merge_dom_value' => '0',
+	'audited' => true,
+	'reportable' => true,
+	'required' => true,
+	'unified_search' => false,
+	'merge_filter' => 'disabled',
+);
+?>
