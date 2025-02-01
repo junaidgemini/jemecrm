763c2733db1adba1f6a2d1bb2334642f623fdcc9
junaidgemini <junaid.gemini24@gmail.com>
2025-02-01 11:00:38 +0500

    remove defauld menu of tickets

--- custom/Extension/modules/jd_tickets/Ext/Menus/freshdeskTickets.php
+++ custom/Extension/modules/jd_tickets/Ext/Menus/freshdeskTickets.php
@@ -1,7 +1,7 @@
 <?php
 global $module_menu;
+unset($module_menu);
 $module_menu[]=Array("index.php?module=jd_tickets&action=displayTickets", "Show Tickets", "List", "jd_tickets");
 $module_menu[]=Array("index.php?module=jd_tickets&action=FCUBSCustomerService", "FCUBS Customer Service", "List", "jd_tickets");
 $module_menu[]=Array("index.php?module=jd_tickets&action=FCUBSAccService", "FCUBS Acc Service", "List", "jd_tickets");
-
 
